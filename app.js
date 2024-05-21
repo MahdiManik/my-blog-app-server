9fa77deb0b11b0b482ee5e9da6be6c0c478de184
Mahdi <mahdimanik994@gmail.com>
2024-05-21 22:06:02 +0600

    setup cors

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./routes/posts");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
+// parser
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1", router);
 
+// this is just for checking
 router.get("/", (req, res) => {
   res.send("Welcome to the Blog API");
 });
